b3fcc940d89434fc2c12c1a4cb9eec7fdebcfed4
william <williamsilvaec@gmail.com>
2023-02-11 14:35:52 -0300

    Initial commit

---
+++ src/test/java/br/com/williamsilva/democacheredis/DemoCacheRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.williamsilva.democacheredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCacheRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
